563b9857671e18a9013554909f5a3a98422076e8
TonyDMorris <anthonymorris86@googlemail.com>
2019-03-08 09:55:07 +0000

    Updates

---
+++ precache-manifest.7f49000f3d4b9d4ebe32f22edff25fc1.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "da9338035ffac54962a0",
+    "url": "/testPaella/static/css/main.2751977d.chunk.css"
+  },
+  {
+    "revision": "da9338035ffac54962a0",
+    "url": "/testPaella/static/js/main.0827dc23.chunk.js"
+  },
+  {
+    "revision": "1641ab39aa70699a1660",
+    "url": "/testPaella/static/js/runtime~main.c0777e6f.js"
+  },
+  {
+    "revision": "cbe4c8284c315ace4a5c",
+    "url": "/testPaella/static/js/2.0e1a3a8d.chunk.js"
+  },
+  {
+    "revision": "8ccc060a4ecb1b64cf5b1fff49540fdd",
+    "url": "/testPaella/static/media/paella-fuse-5741e40c5f9b58723db9a309.8ccc060a.jpg"
+  },
+  {
+    "revision": "55a26bbf31957cbdf8e6827285322556",
+    "url": "/testPaella/static/media/001-chorizo.55a26bbf.svg"
+  },
+  {
+    "revision": "dbbc91905af6920c56a8816df068ebfe",
+    "url": "/testPaella/static/media/002-ham.dbbc9190.svg"
+  },
+  {
+    "revision": "3ad39c0ae4862d0d745713d0e121d84e",
+    "url": "/testPaella/static/media/003-steak.3ad39c0a.svg"
+  },
+  {
+    "revision": "f1d3407c345b2edde1000a86c0c2f414",
+    "url": "/testPaella/static/media/004-pineapple.f1d3407c.svg"
+  },
+  {
+    "revision": "6eed5b62c49f9faa11d201bce314286b",
+    "url": "/testPaella/static/media/005-cheese.6eed5b62.svg"
+  },
+  {
+    "revision": "ba199f9c284e33d57667878570962b66",
+    "url": "/testPaella/static/media/005-tomato.ba199f9c.svg"
+  },
+  {
+    "revision": "e1631c506237067f64e40acb3fb620a7",
+    "url": "/testPaella/static/media/006-garlic.e1631c50.svg"
+  },
+  {
+    "revision": "c7a6c7393ddb0db485f8c882b66558dc",
+    "url": "/testPaella/static/media/006-salami.c7a6c739.svg"
+  },
+  {
+    "revision": "2fabbb639bc621d70b6b47e9f344b60e",
+    "url": "/testPaella/static/media/007-paprika.2fabbb63.svg"
+  },
+  {
+    "revision": "813befdf6191d1b21a8fd1c34f8c2a54",
+    "url": "/testPaella/static/media/007-pepperoni.813befdf.svg"
+  },
+  {
+    "revision": "3d02a39de797977b62c50fc66d4f3333",
+    "url": "/testPaella/static/media/008-salmon.3d02a39d.svg"
+  },
+  {
+    "revision": "39e3f956ad68e7e3a8cb392a3863a89b",
+    "url": "/testPaella/static/media/chicken.39e3f956.svg"
+  },
+  {
+    "revision": "c20d58d2717dff9cfd06e37980b07c71",
+    "url": "/testPaella/static/media/greenbeans.c20d58d2.svg"
+  },
+  {
+    "revision": "cec7e7546e6759fce71789d63b9b51d4",
+    "url": "/testPaella/static/media/rabbit.cec7e754.svg"
+  },
+  {
+    "revision": "0f7474733e57fa5470dd64f1c3ca0a26",
+    "url": "/testPaella/static/media/saffron.0f747473.svg"
+  },
+  {
+    "revision": "02884784d017a0aca3c8bbd6f7c570df",
+    "url": "/testPaella/static/media/whitebeans.02884784.svg"
+  },
+  {
+    "revision": "b441683d410ddca03d117caf1b3a4f2d",
+    "url": "/testPaella/index.html"
+  }
+];
